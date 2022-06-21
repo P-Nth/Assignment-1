970235cf2c414f20e52a9606800077a581cac910
Phyll <103094841+P-Nth@users.noreply.github.com>
2022-06-21 01:52:13 +0000

    Added Button Functionality

--- Hello World!/assets/main.js
+++ Hello World!/assets/main.js
@@ -1,1 +1,4 @@
-alert("Hello World!");
+document.querySelector("#show").addEventListener("click", (e) => {
+  document.querySelector("#disp_name").innerHTML = document.querySelector("input").value;
+  console.log(document.querySelector("input").value);
+});
